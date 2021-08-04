d6993785b6c2bd08b4d5a91e62436cada1a43e61
_Benjvvp <BPerez51511a@gmail.com>
2021-08-03 20:51:53 -0400

    ,,

--- src/public/js/mobile.js
+++ src/public/js/mobile.js
@@ -1,7 +1,8 @@
 const ua = detect.parse(navigator.userAgent);
 
 if(ua.device !== null){
-    if(ua.device.family == 'iPhone' || ua.device.family == 'Android'){
+    console.log(ua.device.family)
+    if(ua.device.family === 'iPhone' || ua.device.family === 'Android'){
         window.location.replace("https://cityforplay.herokuapp.com/phone");
     }
 }
