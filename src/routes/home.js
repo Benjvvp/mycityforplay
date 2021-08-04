c424be58f7cc59caecaf062cb5cdd89d003e322d
_Benjvvp <BPerez51511a@gmail.com>
2021-08-04 09:02:21 -0400

    filter in games

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const Game = require('../models/game')
 
-/* Router GET */
+//------------ Router Get ------------//
 router.get('/', async (req, res) => {
-    res.render('index', {title: 'City For Play ~ Inicio'});
+    res.render('index', {
+        title: 'City For Play ~ Inicio'
+    });
 })
 
 router.get('/phone', async (req, res) => {
-    res.render('phone', {title: 'City For Play ~ Phone Block'});
+    res.render('phone', {
+        title: 'City For Play ~ Phone Block'
+    });
 })
+
+router.get('/about', async (req, res) => {
+    res.render('about', {
+        title: 'City For Play ~ Acerca'
+    });
+})
+
+router.get('/games', async (req, res) => {
+    const games = await Game.find();
+    res.render('games', {
+        games,
+        title: 'City For Play ~ Juegos'
+    });
+})
+
+router.get('/games/:id', async (req, res) => {
+    let game = await Game.findById(req.params.id);
+    game.stats.views++;
+    game.save();
+    res.render('game', {
+        game,
+        title: `City For Play ~ ${game.name}`
+    });
+});
+
+//------------ Post ------------//
+router.post('/addgame', async (req, res) => {
+    const newGame = new Game({
+        name: req.body.name,
+        img: req.body.img,
+        description: req.body.description
+    })
+    await newGame.save();
+    res.redirect('addgame')
+});
+
+
+//------------ Games Filters ------------//
+
+router.get('/games/filter/name', async (req, res) => {
+    const games = await Game.find().sort( { name: -1 } );
+    res.render('games', {
+        games,
+        title: 'City For Play ~ Juegos'
+    });
+})
+
+router.get('/games/filter/calification', async (req, res) => {
+    const games = await Game.find().sort( { stars: -1 } );
+    res.render('games', {
+        games,
+        title: 'City For Play ~ Juegos'
+    });
+})
+
+router.get('/games/filter/views', async (req, res) => {
+    const games = await Game.find().sort( { views: -1 } );
+    res.render('games', {
+        games,
+        title: 'City For Play ~ Juegos'
+    });
+})
+
+router.get('/games/filter/comments', async (req, res) => {
+    const games = await Game.find().sort( { comment: -1 } );
+    res.render('games', {
+        games,
+        title: 'City For Play ~ Juegos'
+    });
+})
+
 module.exports = router;
--- src/routes/web.js
+++ src/routes/web.js
@@ -3,32 +3,14 @@ const router = express.Router();
 const {
     ensureAuthenticated
 } = require('../config/checkAuth')
-const Game = require('../models/game')
 
-/* AREA PRIVADA */
+
+//------------ Authenticated ZONE ------------//
 router.use((req, res, next) => {
     ensureAuthenticated(req, res, next);
 });
 
-
-router.get('/about', async (req, res) => {
-    res.render('about', {title: 'City For Play ~ Acerca'});
-})
-
-router.get('/games', async (req, res) => {
-    const games = await Game.find();
-    console.log(games)
-    res.render('games', {games, title: 'City For Play ~ Juegos'});
-})
-
-router.get('/games/:id', async (req, res) => {
-    let game = await Game.findById(req.params.id);
-    game.stats.views++;
-    game.save();
-    res.render('game', {game, title: `City For Play ~ ${game.name}`});
-});
-
-/* ADMIN */
+//------------ ADMIN ------------//
 
 router.get('/addgame', (req, res, next) => {
     if(req.user.admin){
@@ -36,17 +18,6 @@ router.get('/addgame', (req, res, next) => {
     }else{
         res.redirect('/')
     }
-});
-
-/* POST */
-router.post('/addgame', async (req, res) => {
-    const newGame = new Game({
-        name: req.body.name,
-        img: req.body.img,
-        description: req.body.description
-    })
-    await newGame.save();
-    res.redirect('addgame')
 });
 
 module.exports = router;
